2c4fd2fb11d8cc59afc6e25752d4cc10b70235e0
Dionuta <diounta@gmail.com>
2019-02-02 12:57:02 -0800

    mousedown

--- js/index.js
+++ js/index.js
@@ -1,15 +1,57 @@
 // Your code goes here
 
-//mouseover
-let homeContainer = document.querySelector('.text-content');
+//clown style 
 
-homeContainer.addEventListener("mouseover", ( event ) => {  
-    event.target.style.color= "orange";
-});
-
-homeContainer.addEventListener("mouseout", ( event ) => {  
-    event.target.style.color = "black";
-});
+//things being selected 
+const allButtonsOnPage = document.querySelectorAll('.btn');
+const allImage = document.querySelectorAll('img');
+const body = document.querySelector('body');
 
 
-//keydown
+console.log(body);
+
+//click
+
+
+for (let i = 0; i < allButtonsOnPage.length; i++) {  //loops every button on page and on click adds background color red 
+  allButtonsOnPage[i].addEventListener('click', event => {
+    event.target.style.backgroundColor = 'red';
+  });
+}
+
+//dblclick
+
+for (let i = 0; i < allButtonsOnPage.length; i++) {  
+    allButtonsOnPage[i].addEventListener('dblclick', event => {  //loops every button on page and on double click adds background color rebeccapurple
+      event.target.style.backgroundColor = 'rebeccapurple';
+    });
+  }
+
+  //mouseenter
+ 
+    allImage[0].addEventListener('mouseenter', event => {  //loops every img on page and on mouse enter adds image
+      event.target.setAttribute('src', 'https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80');
+    });
+
+
+  //mouseout 
+    allImage[0].addEventListener('mouseleave', event => {  
+      event.target.setAttribute('src', 'https://i.kym-cdn.com/entries/icons/original/000/000/091/TrollFace.jpg');
+    });
+
+
+ //mousedown
+
+
+ function getRandomColor() {  //random color
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  body.addEventListener('mousedown' , event => {
+     event.target.style.backgroundColor = getRandomColor();
+  })
