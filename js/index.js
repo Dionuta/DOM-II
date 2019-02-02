1013d4631fbff75d9d40dceaeac66fc8720e0f8c
Dionuta <diounta@gmail.com>
2019-02-01 18:46:40 -0800

    sTART

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+//mouseover
+let homeContainer = document.querySelector('.text-content');
+
+homeContainer.addEventListener("mouseover", ( event ) => {  
+    event.target.style.color= "orange";
+});
+
+homeContainer.addEventListener("mouseout", ( event ) => {  
+    event.target.style.color = "black";
+});
+
+
+//keydown
